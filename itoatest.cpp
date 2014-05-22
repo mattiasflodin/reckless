7924c619feef3b729b703133ea6c9f78c43ce293
Mattias Flodin <flodinm@acm.org>
2014-05-22 22:05:24 +0200

    WIP

--- itoatest.cpp
+++ itoatest.cpp
@@ -1,6 +1,51 @@
 #include <cmath>
+#include <cstdint>
+#include <iostream>
 
-void foo(double value, unsigned significant_digits, char* str)
+// Situations:
+// 
+//   0.zzzzxxxxx
+//   power = #z
+//   sig = #x
+//   size = 2+power+digits
+//   value < 1.0
+//
+//   xxxxxzzzz
+//   power = #z + #x
+//   sig = #x
+//   size = power
+//   power >= sig
+//   value >= 1.0
+//   
+//   xxxx.yyyy
+//   power = #x
+//   sig = #x + #y
+//   size = 1 + sig
+//   sig > power
+//   value >= 1.0
+
+int magnitude(double value)
+{
+    int exponent = std::ilogb(value);
+    exponent /= 3;
+    return 0;
+}
+
+void foo_fraction(double value, unsigned sig)
+{
+    double log10 = std::log10(value);
+    double power = std::floor(log10);
+    int ipower = static_cast<int>(power);
+    return;
+}
+
+void foo(double value, unsigned sig)
+{
+    if(value<1.0)
+        foo_fraction(value, sig);
+}
+
+void foo_old(double value, unsigned significant_digits, char* str)
 {
     // double has 53 bits of mantissa, i.e. 53 significant digits. In decimal,
     // that translates to 53*log(2)/log(10) = 15.95 significant digits.
@@ -21,13 +66,41 @@ void foo(double value, unsigned significant_digits, char* str)
         power = std::floor(log10);
     else
         power = std::ceil(log10);
+
+    int ipower = static_cast<int>(power);
     
+    double roundoff_factor = std::pow(10, significant_digits);
+    double nvalue = value*roundoff_factor*std::pow(10, -power);
+    static_assert(sizeof(long)>=8, "long is too small");
+    std::uint_fast64_t ivalue = std::lrint(nvalue);
 
-
+    std::cout << ivalue;
+    for(int i=significant_digits; i!=ipower; ++i)
+    {
+        std::cout << '0';
+    }
+    std::cout << std::endl;
+    return;
 }
 
 int main()
 {
-    foo(12345678.0);
+    char buf[20];
+    magnitude(8);
+    magnitude(9);
+    magnitude(12);
+    magnitude(15.999999999);
+    magnitude(16);
+    magnitude(0.0010000000);
+    magnitude(0.0012345678);
+    magnitude(0.0050000000);
+    magnitude(0.0099999999);
+    magnitude(0.0099999999999999999999999999999);
+    magnitude(0.0000000001);
+    magnitude(0.0000000005);
+    magnitude(0.0000000009);
+    //foo(1234567800000000.0, 6, buf);
+    //foo(0.01234567800000000, 6, buf);
+    //foo(0.0012345678, 6);
     return 0;
 }
