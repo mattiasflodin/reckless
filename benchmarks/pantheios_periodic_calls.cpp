ec3e51e757091d367871506b7b53b7abd8db9daa
Mattias Flodin <flodinm@acm.org>
2015-02-18 20:54:52 +0100

    WIP

---
+++ benchmarks/pantheios_periodic_calls.cpp
@@ -0,0 +1,12 @@
+#include <pantheios/pantheios.hpp>
+#include <pantheios/frontends/stock.h>
+
+#define BENCHMARK_INIT()
+    
+#define BENCHMARK_CLEANUP()
+
+#define LOG( c, i, f ) pantheios::log_INFORMATIONAL( "Hello World! " )
+
+#include "benchmark_main.hpp"
+
+
