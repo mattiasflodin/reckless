65b21ee38e38ea403bd75de85504fdee076345f2
Mattias Flodin <git@codepentry.com>
2015-02-26 21:18:26 +0100

    WIP

--- benchmarks/mandelbrot.cpp
+++ benchmarks/mandelbrot.cpp
@@ -1,5 +1,26 @@
 #include "mandelbrot.hpp"
 
+unsigned const WIDTH_PIXELS=1024;
+unsigned const HEIGHT_PIXELS=768;
+unsigned const MAX_ITERATIONS = 16384;
+unsigned const THREAD_COUNT = 4;
+unsigned const THREAD_SLICE_SIZE = 64;
+double const IN_X1 = -0.74887649041642;
+double const IN_X2 = -0.7488746477625337;
+double const IN_Y1 = 0.0740314695595073;
+double const IN_Y2 = 0.07403331221339358;
+
+double const BOX_CENTER_X = (IN_X1+IN_X2)/2-0.0000001;
+double const BOX_CENTER_Y = (IN_Y1+IN_Y2)/2+0.0000001;
+double const BOX_WIDTH = (IN_X2 - IN_X1)/100;
+double const BOX_HEIGHT = BOX_WIDTH*HEIGHT_PIXELS/WIDTH_PIXELS;
+double const BOX_LEFT = BOX_CENTER_X - BOX_WIDTH/2;
+double const BOX_TOP = BOX_CENTER_Y - BOX_HEIGHT/2;
+
+unsigned g_image[WIDTH_PIXELS*HEIGHT_PIXELS];
+std::size_t g_pixel_index = 0;
+std::mutex g_pixel_index_mutex;
+
 int main()
 {
     mandelbrot();
--- benchmarks/mandelbrot.hpp
+++ benchmarks/mandelbrot.hpp
@@ -10,33 +10,16 @@
 
 #include <fstream>
 
-unsigned const WIDTH_PIXELS=1024;
-unsigned const HEIGHT_PIXELS=768;
-unsigned const MAX_ITERATIONS = 16384;
-unsigned const THREAD_COUNT = 4;
-unsigned const THREAD_SLICE_SIZE = 64;
-double const IN_X1 = -0.74887649041642;
-double const IN_X2 = -0.7488746477625337;
-double const IN_Y1 = 0.0740314695595073;
-double const IN_Y2 = 0.07403331221339358;
-
-double const BOX_CENTER_X = (IN_X1+IN_X2)/2-0.0000001;
-double const BOX_CENTER_Y = (IN_Y1+IN_Y2)/2+0.0000001;
-double const BOX_WIDTH = (IN_X2 - IN_X1)/100;
-double const BOX_HEIGHT = BOX_WIDTH*HEIGHT_PIXELS/WIDTH_PIXELS;
-double const BOX_LEFT = BOX_CENTER_X - BOX_WIDTH/2;
-double const BOX_TOP = BOX_CENTER_Y - BOX_HEIGHT/2;
-
-unsigned g_image[WIDTH_PIXELS*HEIGHT_PIXELS];
-std::size_t g_pixel_index = 0;
-std::mutex g_pixel_index_mutex;
-
-void mandelbrot_thread()
+void mandelbrot_thread(unsigned* sample_buffer,
+        unsigned samples_width, unsigned samples_height,
+        double x1, double y1, double x2, double y2,
+        )
 {
+    unsigned const total_samples;
     while(true)
     {
-        std::size_t pixel_index;
-        std::size_t end_pixel_index;
+        std::size_t sample_index;
+        std::size_t end_sample_index;
         {
             std::lock_guard<std::mutex> lk(g_pixel_index_mutex);
             pixel_index = g_pixel_index;
