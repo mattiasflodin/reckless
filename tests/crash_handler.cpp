a1d0d118528212a9b7f87d0e30c680483f5b424e
Mattias Flodin <git@codepentry.com>
2016-04-30 11:57:17 +0200

    Remove extraneous newline in crash-handler test

--- tests/crash_handler.cpp
+++ tests/crash_handler.cpp
@@ -9,7 +9,7 @@ int main()
     reckless::scoped_crash_handler crash_handler({&g_log});
     reckless::file_writer writer("log.txt");
     g_log.open(&writer);
-    g_log.write("Hello World!\n");
+    g_log.write("Hello World!");
     char* p = nullptr;
     *p = 0;
     return 0;
