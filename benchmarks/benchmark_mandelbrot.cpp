b1db05d8ab5e8598e7e273ccfc06eb46fe8aac09
Mattias Flodin <git@codepentry.com>
2016-08-05 23:06:12 +0200

    Prevent LF -> CRLF translation when writing mandelbrot image

--- benchmarks/benchmark_mandelbrot.cpp
+++ benchmarks/benchmark_mandelbrot.cpp
@@ -36,7 +36,7 @@ int main()
     color_mandelbrot(image, &sample_buffer[0], SAMPLES_WIDTH, SAMPLES_HEIGHT,
             MAX_ITERATIONS);
     
-    std::ofstream ofs("bench_mandelbrot.data");
+    std::ofstream ofs("bench_mandelbrot.data", std::ios::binary);
     ofs.write(image, sizeof(image));
     return 0;
 }
