2c32a338c9304d0c542bdbcee1701600122156dc
Mattias Flodin <git@codepentry.com>
2019-01-06 11:53:26 +0100

    Enable trace logging in single-threaded benchmarks

--- reckless/src/Tupfile.lua
+++ reckless/src/Tupfile.lua
@@ -1,7 +1,7 @@
 if tup.getconfig('TRACE_LOG') != '' then
   table.insert(OPTIONS.define, 'RECKLESS_ENABLE_TRACE_LOG')
-  table.insert(OPTIONS.includes, '../../performance_log/include')
 end
+table.insert(OPTIONS.includes, '../../performance_log/include')
 for i, name in ipairs(filter_platform_files(tup.glob("*.cpp"))) do
   compile(name)
 end
