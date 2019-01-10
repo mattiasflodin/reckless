d8680e0dac850c1ed43a9a80757c55f79da6e697
Mattias Flodin <git@codepentry.com>
2019-01-10 20:26:12 +0100

    WIP move this code

--- benchmarks/periodic_calls.cpp
+++ benchmarks/periodic_calls.cpp
@@ -16,6 +16,48 @@ int main()
     unlink("log.txt");
     performance_log::logger<4096, performance_log::rdtscp_cpuid_clock> performance_log;
 
+// TODO move this to separate program for measuring rdtsc behavior with
+// frequency scaling
+// #ifdef RECKLESS_ENABLE_TRACE_LOG
+//     struct idle_tick_event :
+//         public reckless::detail::timestamped_trace_event
+//     {
+//         std::string format() const
+//         {
+//             return timestamped_trace_event::format() + " idle_tick";
+//         }
+//     };
+//     struct busy_tick_event :
+//         public reckless::detail::timestamped_trace_event
+//     {
+//         std::string format() const
+//         {
+//             return timestamped_trace_event::format() + " busy_tick";
+//         }
+//     };
+
+
+//     usleep(1000000);
+//     for(int i=0; i!=10; i++) {
+//         usleep(1000000);
+//         RECKLESS_TRACE(idle_tick_event);
+//     }
+//     struct timeval start, now;
+//     gettimeofday(&start, nullptr);
+//     do {
+//         gettimeofday(&now, nullptr);
+
+//     } while(now.tv_sec == start.tv_sec);
+//     for(int i=0; i!=10; i++) {
+//         start = now;
+//         do {
+//             gettimeofday(&now, nullptr);
+
+//         } while(now.tv_sec == start.tv_sec);
+//         RECKLESS_TRACE(busy_tick_event);
+//     }
+// #endif
+
     {
         LOG_INIT();
         performance_log::rdtscp_cpuid_clock::bind_cpu(0);
